24c37b63455fe78f158b668054bc19a9065ccc0c
Ricardo <villarrealricardo21@gmail.com>
2021-03-04 22:38:16 -0500

    explore transitions

--- explore.js
+++ explore.js
@@ -4,6 +4,34 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('http://localhost:3000/posts')
     .then(res => res.json())
     .then(data => {
-        data.forEach(dish => makePost(dish, dish.title, recipeContainer, true, dish.id));
+        data.forEach((dish, index) => {
+            if(index === 0){
+                makePost(dish, dish.title, recipeContainer, true, 'firstPost')    
+            }
+            else{
+                makePost(dish, dish.title, recipeContainer, true, dish.id)
+            }
+        });
+        
+        window.addEventListener('scroll', () => {
+            let currentY = window.pageYOffset;
+
+            let posts = document.querySelectorAll('.post-cont');
+
+            posts.forEach((post, index) => {
+                var element = post;
+                var position = element.getBoundingClientRect();
+            
+                // checking whether fully visible
+                if(position.top >= 0 && position.bottom <= window.innerHeight) {
+                    console.log('Element is fully visible in screen', post);
+                    element.firstChild.style.opacity = 1;
+                }
+
+            })
+            
+
+        })
     })
+
 })
